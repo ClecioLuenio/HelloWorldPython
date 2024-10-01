af9ef511bd2aa6521f37282b3695ec72961460c8
ClecioLuenio <clecioluenio@gmail.com>
2024-10-01 18:34:03 -0300

    print xxx seja bem vindo

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Hello World em Python')
-
+    x = input("Digite qual seu nome\n")
+    print(f"olá {x} seja bem vindo(a) ao meu algoritmo em python")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
